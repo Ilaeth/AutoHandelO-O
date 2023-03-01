69600118a72375d789a0ea92ec81334d09c2430f
Ilaeth <przemek1999.10.5@wp.pl>
2023-03-01 17:31:35 +0100

    Entry - Main Menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Przemek", 1000000.0);
+
+        System.out.println("                               Hello Auto-Handel");
+        System.out.println("                                                                ░░          ░░\n");
+        System.out.println("                                              ░░░░  ░░          ▒▒  ░░  ▒▒▒▒▓▓▒▒▓▓░░░░\n");
+        System.out.println("                                              ░░                    ▒▒▓▓▒▒▓▓▒▒▓▓▒▒██▓▓▓▓▒▒\n");
+        System.out.println("                                                          ░░░░  ▒▒▒▒░░░░▒▒▓▓░░▒▒▓▓▓▓▒▒▒▒▒▒▓▓\n");
+        System.out.println("                              ░░░░            ░░    ░░    ░░  ░░░░▒▒▒▒░░██▒▒░░░░▒▒▒▒▓▓▒▒▒▒▒▒░░░░\n");
+        System.out.println("                                    ▒▒        ▒▒                ▒▒░░▓▓░░▒▒▓▓████████░░▓▓▓▓▒▒▒▒▓▓░░\n");
+        System.out.println("                                ░░        ░░      ▓▓  ░░    ░░░░▓▓▓▓░░██░░  ▓▓██    ██▒▒▒▒▒▒▒▒██▒▒\n");
+        System.out.println("                                          ▒▒  ░░      ░░  ░░  ▓▓▒▒░░██        ▒▒      ▓▓▒▒▒▒▓▓▓▓▓▓░░                                  ░░\n");
+        System.out.println("                        ░░                    ░░      ░░  ░░▒▒▒▒░░██▓▓      ░░▓▓      ▒▒██▒▒▒▒▓▓▒▒▓▓\n");
+        System.out.println("                                                      ▒▒  ▒▒▒▒░░██  ▓▓▓▓    ▒▒▓▓    ░░██░░▓▓░░▓▓▒▒▒▒▒▒\n");
+        System.out.println("                                        ▒▒          ▒▒  ░░  ▓▓▒▒░░    ██▓▓  ▒▒██  ▒▒██      ▓▓▓▓▓▓▒▒▒▒\n");
+        System.out.println("                                                    ░░  ▓▓░░▒▒██        ██████▓▓████        ▓▓░░▓▓▒▒▓▓\n");
+        System.out.println("                                    ▓▓    ░░        ░░░░▒▒██░░██        ▒▒██▒▒▒▒██▓▓        ▒▒░░▓▓▓▓▓▓░░\n");
+        System.out.println("                          ░░              ░░  ▒▒  ░░  ░░░░  ░░██████████▓▓██    ▓▓▓▓▓▓████████░░▓▓▒▒▒▒\n");
+        System.out.println("                          ░░      ░░        ▒▒    ░░  ░░▒▒▒▒░░▓▓░░  ░░░░████    ████▒▒░░    ██░░▓▓▒▒░░░░\n");
+        System.out.println("                                                        ░░▓▓▒▒██        ▒▒██▒▒████▓▓        ▒▒░░▒▒▒▒▓▓\n");
+        System.out.println("                                                    ▒▒    ▒▒▒▒▓▓      ░░▓▓▓▓▓▓▓▓▒▒██▒▒      ▓▓░░░░▒▒▓▓\n");
+        System.out.println("                                    ░░  ░░      ░░  ░░    ░░▒▒░░▒▒    ██▒▒  ▒▒▓▓  ░░██░░    ▒▒▓▓▒▒▓▓▒▒\n");
+        System.out.println("                                                    ▒▒  ▒▒▒▒▒▒▒▒▓▓▒▒██░░    ▒▒▓▓      ██▒▒██▒▒▒▒▒▒▒▒░░\n");
+        System.out.println("                                              ░░▒▒    ░░  ░░▒▒░░░░██▓▓      ░░▓▓      ▒▒██░░▒▒▓▓▓▓▒▒\n");
+        System.out.println("                      ░░                            ░░    ░░▒▒▓▓▓▓▒▒▓▓░░    ░░▓▓    ░░██░░▓▓▒▒▓▓▒▒\n");
+        System.out.println("                                                  ░░    ██  ░░  ▒▒▒▒▓▓░░██▓▓████▓▓██▒▒▒▒▓▓▓▓▓▓▒▒▒▒\n");
+        System.out.println("                                            ▒▒▒▒    ▒▒      ░░  ░░▓▓▒▒▓▓▒▒▒▒░░░░░░▓▓▒▒▒▒▒▒▒▒░░▒▒\n");
+        System.out.println("                                        ░░          ░░  ▒▒  ░░  ▒▒░░▒▒▒▒▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒\n");
+        System.out.println("                                                                  ░░░░░░  ▓▓▒▒▓▓▒▒██░░▒▒░░\n");
+        System.out.println("                                                                            ░░\n");
+        menu();
+    }
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Main-Menu");
+        System.out.println("");
+
+        System.out.println("1.");
+        System.out.println("2.");
+        System.out.println("3.");
+        System.out.println("4.");
+        System.out.println("5.");
+
+        //switch () {
+
+        //}
     }
 }
