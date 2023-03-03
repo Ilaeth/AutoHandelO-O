2389571dba7eb92dda1f542e887abb0135e23435
Ilaeth <przemek1999.10.5@wp.pl>
2023-03-03 14:18:57 +0100

    Enum

---
+++ src/vehicles/Enum.java
@@ -0,0 +1,23 @@
+package vehicles;
+
+public class Enum {
+    public enum Vehicle {
+        MOTORCYCLE, CAR, BUS, TRUCK
+    }
+
+    public enum Brand {
+        AUDI, BMW, CITROEN, DACIA, FIAT, FORD, HYUNDAI, KIA, MERCEDES, NISSAN, OPEL, PEUGEOT, RENAULT, SEAT, SKODA, TOYOTA, VOLKSWAGEN, VOLVO
+    }
+
+    public enum Mileage {
+        NEW, USED, DAMAGED, WRECKAGE
+    }
+
+    public enum Color {
+        WHITE, BLACK, GRAY, SILVER, BLUE, RED, BROWN, GREEN, ORANGE, BEIGE
+    }
+
+    public enum Segment {
+        PREMIUM, STANDARD, BUDGET
+    }
+}
